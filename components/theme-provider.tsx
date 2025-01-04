5956e0b36a357c9ca1a0c8886b4d6867bf41cfb4
Aji Maulana <mancode700@gmail.com>
2025-01-04 17:55:44 +0700

    UPDATE: vidoe cat and image processing

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,21 +1,16 @@
-// components/theme-provider.tsx
+"use client"
 
-import { ReactNode } from "react";
-import { NextThemesProvider } from "next-themes";
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-interface ThemeProviderProps {
-  children: ReactNode;
-  attribute?: string | string[];  // Memperbaiki tipe agar sesuai dengan yang diharapkan
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  attribute?: string;
   defaultTheme?: string;
   enableSystem?: boolean;
   disableTransitionOnChange?: boolean;
 }
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>
-      {children}
-    </NextThemesProvider>
-  );
-}
-
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+} 
